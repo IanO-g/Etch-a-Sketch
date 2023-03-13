0dcb0fc0bc044d516fe5d5a51d9ea6b4a7d3254f
ojfordinner <124970715+ojfordinner@users.noreply.github.comian.ouyoung@gmail.com>
2023-03-12 22:36:22 -0400

    Added JS to change div background color on mouseover; formatted container to create grid for generated divs

--- logic.js
+++ logic.js
@@ -2,6 +2,11 @@ const container = document.querySelector('.container');
 let containerWidth = container.offsetWidth,
     containerHeight = container.offsetHeight;
 
+function formatContainer(){
+        container.style.cssText = `grid-template-columns:repeat(4, 1fr)`;
+    }
+   
+
 function createDivs(){
 
     gridDiv = document.createElement('div');
@@ -9,12 +14,29 @@ function createDivs(){
     container.appendChild(gridDiv);
     gridDiv.style.width = 1/4 * containerWidth - 2 + 'px' ;
     gridDiv.style.height = 1/4 * containerHeight -2 + 'px';
-    
+
 }
 
+
+
 function production(){
-    for (i = 1; i <=16;i++){
+    for (i = 1; i <= 16; i++){
         createDivs();
     }
 }
+
+formatContainer();
 production();
+
+
+function divDraw(e){
+  
+        e.target.style.backgroundColor = 'black';
+}
+
+const gridDivAll = document.querySelectorAll('.grid');
+
+gridDivAll.forEach(gridDiv => gridDiv.addEventListener(
+    'mouseover',divDraw))
+
+
