d147af18515c983c6b8256c021b1c8697e9e8776
ojfordinner <124970715+ojfordinner@users.noreply.github.comian.ouyoung@gmail.com>
2023-03-14 22:44:59 -0400

    Modified JS to allow for user input

--- logic.js
+++ logic.js
@@ -1,29 +1,44 @@
 const container = document.querySelector('.container');
 let containerWidth = container.offsetWidth,
-    containerHeight = container.offsetHeight;
+    containerHeight = container.offsetHeight,
+    userBtn = document.querySelector('#canvasBtn');
+
+    function validInput(){
+        let canvasNumber = (window.prompt('Choose the size of your canvas',''));
+    if (isNaN(canvasNumber)){
+        alert('please enter a number')
+        validInput()
+    } else if (canvasNumber < 0 || canvasNumber>100){
+        alert('please choose a number between 1 and 100')
+        validInput()
+    } else {
+        console.log(canvasNumber);
+        return Number(canvasNumber);
+    }
+    }
+    const userInput = validInput();
 
 function formatContainer(){
-        container.style.cssText = `grid-template-columns:repeat(4, 1fr)`;
+
+        container.style.cssText = `grid-template-columns:repeat(${userInput}, 1fr)`;
+
     }
-   
 
 function createDivs(){
 
     gridDiv = document.createElement('div');
     gridDiv.classList.add('grid');
     container.appendChild(gridDiv);
-    gridDiv.style.width = 1/4 * containerWidth - 2 + 'px' ;
-    gridDiv.style.height = 1/4 * containerHeight -2 + 'px';
-
+    gridDiv.style.width = 1/userInput * containerWidth - 2 + 'px';
+    gridDiv.style.height = 1/userInput * containerHeight -2 + 'px';
 }
 
-
-
 function production(){
-    for (i = 1; i <= 16; i++){
+    for (i = 1; i <= userInput * userInput; i++){
         createDivs();
     }
 }
+
 
 formatContainer();
 production();
